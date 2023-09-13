8c8c085e08d72448cd3ed1047bafcb1bcd231745
covalent <mail@lukehmcc.com>
2023-09-13 07:55:33 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Windows goodies
+This is my repo for windows scripts
+
+## New install script
+Please run `winget install gsudo` first.
---
+++ new-install.bat
@@ -0,0 +1,17 @@
+@REM Install basic apps
+
+gsudo winget install Bitwarden.Bitwarden
+gsudo winget install Joplin.Joplin
+gsudo winget install deltachat.deltachat
+gsudo winget install Google.AndroidStudio
+gsudo winget install DelugeTeam.Deluge
+gsudo winget install Git.Git
+gsudo winget install acrosync.DuplicacyWeb
+gsudo winget install Mozilla.Firefox
+gsudo winget install Mozilla.Thunderbird --version 102.15
+gsudo winget install Notepad++.Notepad++
+gsudo winget install ONLYOFFICE.DesktopEditors
+gsudo winget install Valve.Steam
+gsudo winget install Element.Element
+gsudo winget install Vup.Vup
+gsudo winget install Cloudflare.Warp
